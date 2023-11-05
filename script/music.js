fda1e919c1eb5085d2a5b522b6a31ac343fdc684
igor3286411 <139371965+igor3286411@users.noreply.github.com>
2023-11-05 10:40:57 +0100

    Add files via upload

--- script/music.js
+++ script/music.js
@@ -1,10 +1,28 @@
+const playList = [
+    {
+        title: 'Aqua Caelestis',
+        src: '../assets/sounds/AquaCaelestis.mp3',
+    },
+    {
+        title: 'River Flows In You',
+        src: '../assets/sounds/RiverFlowsInYou.mp3',
+    }, {
+        title: 'Summer Wind',
+        src: '../assets/sounds/SummerWind.mp3',
+    },
+    {
+        title: 'Ennio Morricone',
+        src: '../assets/sounds/EnnioMorricone.mp3',
+    }
+]
+
 const ulPlayList = document.querySelector('.play-list')
 const play = document.querySelector('.play')
 const playLeft = document.querySelector('.play-prev')
 const playRight = document.querySelector('.play-next')
 let isPlay = false;
 let playNum = 1;
-import playList from './playList.js';
+// import playList from './playList.js';
 
 const audio = new Audio();
 
